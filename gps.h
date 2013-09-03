4c8140263997c00bc07cd8e2c9b9d02896565e7c
Mike <m1ari@m1ari.co.uk>
2013-09-03 11:42:07 +0000

    Added storage for settings

--- gps.h
+++ gps.h
@@ -16,6 +16,13 @@ class GPS {
 		void nmeaGPGGA(char *nmea);	// Parse a GPGGA string
 		void nmeaGPRMC(char *nmea);	// Parse a GPRMC string
 
+		bool deviceopen;		// Is the device open
+		string device;
+		int baud;
+		int bits;
+		int stopbits;
+		char parity;
+
 		// thread id
 		// static void thread setup
 	public:
@@ -26,6 +33,7 @@ class GPS {
 		void Run();			// Run as a seperate thread to get data from the GPS
 		void Stop();			// Exit the run loop
 
+		// Should we make these bools ?
 		void setDevice(string d);
 		void setBaud(int b);
 		void setBits(int b);
