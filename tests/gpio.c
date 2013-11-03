001e1ecd5cf451ef0f37feb405a694b723c9d3ed
Mike <m1ari@m1ari.co.uk>
2013-11-03 17:33:03 +0000

    tidied and updated gpio code for pwm

--- tests/gpio.c
+++ tests/gpio.c
@@ -1,17 +1,23 @@
-//
-//  How to access GPIO registers from C-code on the Raspberry-Pi
-//  Example program
-//  15-January-2012
-//  Dom and Gert
-//  Revised: 15-Feb-2013
-
+/* Access to GPIO Registeres on the Raspberry Pi
+ * Based on example code from
+ *   http://elinux.org/RPi_Low-level_peripherals#C_2
+ * With Additions from
+ *   http://www.frank-buss.de/raspberrypi/pwm.c
+ *   http://www.raspberrypi.org/phpBB3/viewtopic.php?t=8467&p=124620
+ *   http://git.drogon.net/?p=wiringPi;a=blob;f=wiringPi/wiringPi.c
+*/
 
 // Access from ARM Running Linux
-
-#define BCM2708_PERI_BASE        0x20000000
-#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+#define BCM2708_PERI_BASE			     0x20000000
+#define GPIO_BASE		(BCM2708_PERI_BASE + 0x00200000)	/* GPIO Controller */
+#define GPIO_PWM		(BCM2708_PERI_BASE + 0x0020C000)	/* PWM Controller */
+#define CLOCK_BASE		(BCM2708_PERI_BASE + 0x00101000)
+#define GPIO_PADS		(BCM2708_PERI_BASE + 0x00100000)
+#define GPIO_TIMER		(BCM2708_PERI_BASE + 0x0000B000)
 
 #include <stdio.h>
+#include <stdint.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -21,33 +27,39 @@
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
 
-int  mem_fd;
-void *gpio_map;
+volatile unsigned *mapRegisterMemory(int base){
+	static int mem_fd=0;
+	void *map;
 
-void setup_io() {
 	/* open /dev/mem */
-	if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-		printf("can't open /dev/mem \n");
-		exit(-1);
+	if (!mem_fd) {
+		if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+			printf("can't open /dev/mem \n");
+			exit(-1);
+		}
 	}
 
 	/* mmap GPIO */
-	gpio_map = mmap(
-		NULL,						//Any adddress in our space will do
-		BLOCK_SIZE,				//Map length
-		PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-		MAP_SHARED,				//Shared with other processes
-		mem_fd,					//File to map
-		GPIO_BASE				//Offset to GPIO peripheral
+	map = mmap(
+		NULL,
+		BLOCK_SIZE,		// Map length
+		PROT_READ|PROT_WRITE,	// Enable reading & writting to mapped memory
+		MAP_SHARED,		// Shared with other processes
+		mem_fd,			// File to map
+		base			// Offset to GPIO peripheral
 	);
 
-	close(mem_fd); //No need to keep mem_fd open after mmap
-
-	if (gpio_map == MAP_FAILED) {
-		printf("mmap error %d\n", (int)gpio_map);//errno also set!
-		exit(-1);
+	if (map == MAP_FAILED) {
+		printf("mmap error %d\n", (int)map);	// Could also get result of errno
+		exit (-1);
 	}
 
 	// Always use volatile pointer!
-	gpio = (volatile unsigned *)gpio_map;
+	return (volatile unsigned *)map;
+}
+
+void setup_io(){
+	gpio = mapRegisterMemory(GPIO_BASE);
+	pwm = mapRegisterMemory(GPIO_PWM);
+	clk = mapRegisterMemory(CLOCK_BASE);
 }
--- tests/gpio.h
+++ tests/gpio.h
@@ -1,5 +1,7 @@
 // I/O access
 volatile unsigned *gpio;
+volatile unsigned *pwm;
+volatile unsigned *clk;
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
@@ -8,5 +10,33 @@ volatile unsigned *gpio;
 
 #define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
 #define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+// Defines for managing PWM (Taken from WiringPi)
+#define PWM_CONTROL 0
+#define PWM_STATUS  1
+#define PWM0_RANGE  4
+#define PWM0_DATA   5
+#define PWM1_RANGE  8
+#define PWM1_DATA   9
+
+#define PWMCLK_CNTL 40
+#define PWMCLK_DIV  41
+
+#define PWM0_MS_MODE    0x0080  // Run in MS mode
+#define PWM0_USEFIFO    0x0020  // Data from FIFO
+#define PWM0_REVPOLAR   0x0010  // Reverse polarity
+#define PWM0_OFFSTATE   0x0008  // Ouput Off state
+#define PWM0_REPEATFF   0x0004  // Repeat last value if FIFO empty
+#define PWM0_SERIAL     0x0002  // Run in serial mode
+#define PWM0_ENABLE     0x0001  // Channel Enable
+
+#define PWM1_MS_MODE    0x8000  // Run in MS mode
+#define PWM1_USEFIFO    0x2000  // Data from FIFO
+#define PWM1_REVPOLAR   0x1000  // Reverse polarity
+#define PWM1_OFFSTATE   0x0800  // Ouput Off state
+#define PWM1_REPEATFF   0x0400  // Repeat last value if FIFO empty
+#define PWM1_SERIAL     0x0200  // Run in serial mode
+#define PWM1_ENABLE     0x0100  // Channel Enable
+
 
 void setup_io();
