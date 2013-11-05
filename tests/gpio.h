46a4119a24d0b44bc676f710bd11bfe34a8ae164
Mike <m1ari@m1ari.co.uk>
2013-11-05 12:07:59 +0000

    Added BCM_PASSWORD and tidied formatting

--- tests/gpio.h
+++ tests/gpio.h
@@ -4,9 +4,9 @@ volatile unsigned *pwm;
 volatile unsigned *clk;
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
-#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
-#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
-#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+#define INP_GPIO(g)       *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))	// Set bits to 0 Input
+#define OUT_GPIO(g)       *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+((g)/10)) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
 #define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
 #define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
@@ -38,5 +38,7 @@ volatile unsigned *clk;
 #define PWM1_SERIAL     0x0200  // Run in serial mode
 #define PWM1_ENABLE     0x0100  // Channel Enable
 
+//BCM Magic
+#define BCM_PASSWORD            0x5A000000
 
 void setup_io();
